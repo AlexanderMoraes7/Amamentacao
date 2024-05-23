44fab637a7cc909a1240dfaafe1d6bb95837fdd0
Alex <alexander97643@gmail.com>
2024-05-22 21:54:21 -0300

    modified:   bin/apache/logs/error.log 	modified:   bin/apache/logs/httpd.pid 	modified:   bin/database/mysql-5.7.11/data/Alexander.pid 	modified:   bin/database/mysql-5.7.11/data/ib_logfile0 	modified:   bin/database/mysql-5.7.11/data/ibdata1 	modified:   bin/database/mysql-5.7.11/data/ibtmp1 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_index_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_table_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.ibd 	modified:   www/Controller/unClasseCadastro.php 	modified:   www/Controller/unClasseLogin.php 	modified:   www/View/index.html 	modified:   bin/apache/logs/error.log

--- www/Controller/unClasseCadastro.php
+++ www/Controller/unClasseCadastro.php
@@ -7,18 +7,15 @@ $email=$_POST["email"];
 $senha1=$_POST["senha1"];
 $senha2=$_POST["senha2"];
 
+$nome = strtolower($nome);
 $email = strtolower($email);
 
 if ($senha1 <> $senha2){
     echo "As senhas não são iguais. Tente novamente.";
 }
 else{
+    $senha1 = md5($senha1);
     $pdo->exec("INSERT INTO USUARIOS(nome, email, senha) VALUES 
-            ('$nome','$email', (SELECT md5('$senha1')))");
+            ('$nome','$email', $senha1))");
     echo "Conta criada com sucesso";
 }
-
-// $resultado = $pdo->query("SELECT * FROM USUARIOS");
-// $resultado->fetchAll(PDO::FETCH_ASSOC);
-// echo($resultado);
-// exit();
--- www/Controller/unClasseLogin.php
+++ www/Controller/unClasseLogin.php
@@ -4,16 +4,25 @@ require('../Model/unClasseConfig.php');
 
 $email=$_POST["email"];
 $senha=$_POST["senha"];
+$email = strtolower($email);
+$senha = md5($senha);
 
+$resultado = $pdo->query("SELECT email, senha FROM USUARIOS WHERE email = '$email'");
+if (!$resultado) {
+    echo "E-mail ou senha não encontrados" . $pdo->errorInfo()[2];
+}
+else {
+    $dados = $resultado->fetchAll(PDO::FETCH_ASSOC);
+}
 
-if ($senha1 <> $senha2){
-    echo "As senhas não são iguais. Tente novamente.";
+if ($dados[0] == "") {
+    echo "E-mail não encontrado";
+}
+
+if (($dados[0]["email"] == $email) and ($dados[0]["senha"] == $senha)){
+    echo("e-mail e senha igual ");
 }
 else{
-    $pdo->exec("SELECT EMAIL, SENHA FROM USUARIOS");
+    echo("e-mail ou senha são diferentes");
 }
-
-// $resultado = $pdo->query("SELECT * FROM USUARIOS");
-// $resultado->fetchAll(PDO::FETCH_ASSOC);
-// echo($resultado);
-// exit();
+exit();
