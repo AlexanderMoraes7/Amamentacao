cbe1207c921ed7d0a3dad7f105f285efeb903c5c
Alex <alexander97643@gmail.com>
2024-05-24 22:47:47 -0300

    modified:   bin/apache/conf/extra/httpd-autoindex.conf 	modified:   bin/apache/conf/extra/httpd-dav.conf 	modified:   bin/apache/conf/extra/httpd-multilang-errordoc.conf 	modified:   bin/apache/conf/extra/httpd-ssl.conf 	modified:   bin/apache/conf/httpd.conf 	modified:   bin/apache/logs/error.log 	modified:   bin/apache/logs/httpd.pid 	modified:   bin/apache/php.ini 	modified:   bin/database/mysql-5.7.11/data/Alexander.pid 	modified:   bin/database/mysql-5.7.11/data/ib_logfile0 	modified:   bin/database/mysql-5.7.11/data/ibdata1 	modified:   bin/database/mysql-5.7.11/data/ibtmp1 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_index_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_table_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.ibd 	modified:   bin/database/mysql-5.7.11/my.ini 	modified:   www/View/cadastro.html 	modified:   www/View/index.html 	modified:   www/View/paginaprincipal.php 	new file:   www/View/perfil.html 	renamed:    www/View/cadastro.css -> www/css/cadastro.css 	renamed:    www/View/index.css -> www/css/index.css 	new file:   www/css/paginaprincipal.css 	new file:   www/css/perfil.css

--- www/View/paginaprincipal.php
+++ www/View/paginaprincipal.php
@@ -1,5 +1,11 @@
 <?php
-
+/*
+<h1>Bem-vinda, <?php echo $_SESSION['nome']?></h1>
+<h2>Página principal de Apoio a Amamentação 💖</h2>
+<p>
+    <a href="logout.php">Sair</a>
+</p>
+*/
 include("protect.php");
 
 ?>
@@ -8,13 +14,12 @@ include("protect.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/paginaprincipal.css">
     <title>Página Principal - Amamentação</title>
 </head>
 <body>
-    <h1>Bem-vinda, <?php echo $_SESSION['nome']?></h1>
-    <h2>Página principal de Apoio a Amamentação 💖</h2>
-    <p>
-        <a href="logout.php">Sair</a>
-    </p>
+    <div class="Principal">
+
+    </div>
 </body>
 </html>
