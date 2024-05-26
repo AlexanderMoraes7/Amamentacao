79e860d178898f3b9599b57b9508aa0c3fe007c3
Alex <alexander97643@gmail.com>
2024-05-26 14:00:09 -0300

    modified:   bin/apache/logs/access.log 	modified:   bin/apache/logs/error.log 	modified:   bin/apache/logs/httpd.pid 	deleted:    bin/apache/sessions/sess_382sftikln667dobm2ualilbf4oi9srq 	modified:   bin/apache/sessions/sess_62s2n6m1jsht510geguia4fu24 	modified:   bin/database/mysql-5.7.11/data/Alexander.pid 	modified:   bin/database/mysql-5.7.11/data/ib_logfile0 	modified:   bin/database/mysql-5.7.11/data/ibdata1 	modified:   bin/database/mysql-5.7.11/data/ibtmp1 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_index_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_table_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.frm 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.ibd 	modified:   www/Controller/unClasseCadastro.php 	modified:   www/View/cadastro.html 	modified:   www/View/paginaprincipal.php 	modified:   www/css/paginaprincipal.css

--- www/Controller/unClasseCadastro.php
+++ www/Controller/unClasseCadastro.php
@@ -2,14 +2,21 @@
 require('../Model/unClasseConfig.php');
 
 $nome=$mysqli->real_escape_string($_POST["nome"]);
+$sobrenome=$mysqli->real_escape_string($_POST["sobrenome"]);
 $email=$mysqli->real_escape_string($_POST["email"]);
 $senha1=$mysqli->real_escape_string($_POST["senha1"]);
 $senha2=$mysqli->real_escape_string($_POST["senha2"]);
+$nome = strtolower($nome);              // Converte tudo para minúscula
+$nome = ucfirst($nome);                 // Converte primeira letra para maiúscula
+$sobrenome = strtolower($sobrenome);
+$sobrenome = ucfirst($sobrenome);
 $email = strtolower($email);
 
 try {
     if (strlen($nome) == 0) {
         echo "Nome não pode ser vazio";
+    } elseif (strlen($sobrenome) == 0) {
+        echo "Sobrenome não pode ser vazio";
     } elseif (strlen($email) == 0) {
         echo "E-mail não pode ser vazio";
     } elseif ((strlen($senha1) == 0) or (strlen($senha2) == 0)) {
@@ -18,8 +25,8 @@ try {
         echo "As senhas não são iguais. Digite novamente.";
     } else{
         $senha1 = password_hash($senha1, PASSWORD_DEFAULT);
-        $mysqli->query("INSERT INTO USUARIOS(nome, email, senha) VALUES 
-                ('$nome','$email', '$senha1')");
+        $mysqli->query("INSERT INTO USUARIOS(nome, sobrenome, email, senha) VALUES 
+                ('$nome', '$sobrenome','$email', '$senha1')");
         echo "Conta criada com sucesso";
         }
     } catch (PDOException $e) {
--- www/View/paginaprincipal.php
+++ www/View/paginaprincipal.php
@@ -1,12 +1,15 @@
 <?php
 /*
 
-
 <p>
     <a href="logout.php">Sair</a>
 </p>
 */
 include("protect.php");
+
+if(!isset($_SESSION["nome"])) {
+    die("Você não está autorizado a acessar essa página.<p><a href=\"index.html\">Logar</a></p>");
+}
 
 ?>
 
@@ -16,19 +19,20 @@ include("protect.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/paginaprincipal.css">
-    <title>Página Principal - Amamentação</title>
+    <title>Página Principal</title>
 </head>
 <body>
 <div class="Container">
         <div class="Top">
             <h1>
-                Apoio á Amamentação, <?php echo $_SESSION['nome']?></h1>
+                Bem-vinda, <?php echo $_SESSION['nome'] . '!'?> <br>
+                Apoio á Amamentação
             </h1>
             <i class="Sino">
-                <img src="../images/icons8-bell-30.png" alt="ícone">
+                <img src="../images/icons8-bell-30.png" alt="Notificações">
             </i>
             <i class="Config">
-                <img src="../images/icons8-settings-30.png" alt="">
+                <img src="../images/icons8-settings-30.png" alt="Configurações">
             </i>
         </div>
         <div class="Middle">
