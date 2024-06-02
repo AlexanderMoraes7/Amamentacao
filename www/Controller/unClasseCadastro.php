303c424d052ef282d4765236abc3d22344c0e62a
Alex <alexander97643@gmail.com>
2024-06-02 11:09:55 -0300

    modified:   bin/database/mysql-5.7.11/data/ib_logfile0 	modified:   bin/database/mysql-5.7.11/data/ibdata1 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_index_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_table_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/fotos_perfil.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.ibd 	modified:   www/Controller/unClasseCadastro.php

--- www/Controller/unClasseCadastro.php
+++ www/Controller/unClasseCadastro.php
@@ -3,16 +3,16 @@ include_once("../Model/unClasseConfig.php");
 include_once("../View/variaveis.php");
 
 
-$nome=$mysqli->real_escape_string($_POST["nome"]);
-$sobrenome=$mysqli->real_escape_string($_POST["sobrenome"]);
-$email=$mysqli->real_escape_string($_POST["email"]);
-$senha1=$mysqli->real_escape_string($_POST["senha1"]);
-$senha2=$mysqli->real_escape_string($_POST["senha2"]);
-$nome = strtolower($nome);              // Converte tudo para minúscula
-$nome = ucfirst($nome);                 // Converte primeira letra para maiúscula
-$sobrenome = strtolower($sobrenome);
-$sobrenome = ucfirst($sobrenome);
-$email = strtolower($email);
+$nome       = $mysqli->real_escape_string($_POST["nome"]);
+$sobrenome  = $mysqli->real_escape_string($_POST["sobrenome"]);
+$email      = $mysqli->real_escape_string($_POST["email"]);
+$senha1     = $mysqli->real_escape_string($_POST["senha1"]);
+$senha2     = $mysqli->real_escape_string($_POST["senha2"]);
+$nome       = strtolower($nome);        // Converte tudo para minúscula
+$nome       = ucfirst($nome);           // Converte primeira letra para maiúscula
+$sobrenome  = strtolower($sobrenome);
+$sobrenome  = ucfirst($sobrenome);
+$email      = strtolower($email);
 
 try {
     if (strlen($nome) == 0) {
@@ -27,19 +27,25 @@ try {
         echo "As senhas não são iguais. Digite novamente.";
     } else{
         $senha1 = password_hash($senha1, PASSWORD_DEFAULT);
+        
         //insere usuario na tabela
         $mysqli->query("INSERT INTO USUARIOS(nome, sobrenome, email, senha) VALUES 
                 ('$nome', '$sobrenome','$email', '$senha1')");
+        
         // busca usuario criado
         $BuscaUser = $mysqli->query("SELECT * FROM USUARIOS
                 WHERE email = '$email'") or die($mysqli->error);
         $user = $BuscaUser->fetch_assoc();
         $id = $user["idusuario"];
+        
         // insere usuario na tabela fotos, para inserção de fotos posteriormente
-        $mysqli->query("INSERT INTO FOTOS_PERFIL(idusuario) VALUES
-                ('$id')");
-        echo "Conta criada com sucesso";
-        }
+        if ($mysqli->query("INSERT INTO FOTOS_PERFIL(idusuario) VALUES
+                ('$id')")) {
+                $Mensagem = "Conta criada com sucesso";
+                header("Location: ../View/index.php?msg=" . urldecode($Mensagem));
+                exit();
+            }
+    }
     } catch (PDOException $e) {
     echo $e->getMessage();
 }
