a49613fd2ab4a8875c3bcac18c41e89a9dd729e7
Alex <alexander97643@gmail.com>
2024-06-03 18:50:18 -0300

    modified:   bin/apache/logs/access.log 	modified:   bin/apache/logs/error.log 	modified:   bin/apache/logs/httpd.pid 	modified:   bin/database/mysql-5.7.11/data/Alexander.err 	modified:   bin/database/mysql-5.7.11/data/Alexander.pid 	modified:   bin/database/mysql-5.7.11/data/ib_logfile0 	modified:   bin/database/mysql-5.7.11/data/ibdata1 	modified:   bin/database/mysql-5.7.11/data/ibtmp1 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_index_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/mysql/innodb_table_stats.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/fotos_perfil.ibd 	modified:   bin/database/mysql-5.7.11/data/nutrirede/usuarios.ibd 	new file:   www/Controller/unClasseDelete.php 	deleted:    www/Fotos_perfil/665cdce8a3cb4.gif 	deleted:    www/Fotos_perfil/avatars.gif 	modified:   www/View/config.php 	modified:   www/css/config.css

--- www/View/config.php
+++ www/View/config.php
@@ -1,25 +1,26 @@
 <?php
 include_once("../Controller/unClasseProtect.php");
+include_once("../Controller/unClassePerfil.php");
 include_once("variaveis.php");
 include_once("../css/Estilos.php");
 
-$oProtect = new Protect();// instância para permitir o acesso somente a quem estiver logado
+$oProtect = new Protect(); // Certifica que o usuário está logado
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/config.css">
-    <title>Apoio á Amamentação</title>
+    <title>Apoio à Amamentação</title>
     <?php echo $Estilizacao_pagina; // Estilo do Cabeçalho e do rodapé da página ?>
 </head>
 <body>
     <div class="Container">
         <?php echo $Topo ?>
         <div class="Middle">
-        <div class="IdiomaContainer">
+            <div class="IdiomaContainer">
                 <button>
                     Idioma <br>
                     Português
@@ -28,6 +29,9 @@ $oProtect = new Protect();// instância para permitir o acesso somente a quem es
             <a href="notificacoes.php"><button>Notificações</button></a>
             <a href="privacidade.php"><button>Privacidade</button></a>
             <a class="ConfigSistema" href="configsistema.php"><button>Configuração do sistema</button></a>
+            <form action="../Controller/unClasseDelete.php" method="post">
+                <button type="submit">Deletar Conta</button>
+            </form>
             <a href="../Controller/unLogout.php"><button>Sair</button></a>
         </div>
         <?php echo $Bottom ?>
